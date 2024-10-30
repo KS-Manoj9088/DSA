b0e307b8b08604036c0a718bdd014395b20abfcc
KS MANOJ KUMAR <ksmk2104@gmail.com>
2024-10-30 07:44:30 +0530

    Added insertFirst and insertLast methods

--- LinkedLists/DoublyLinkedLists/doublyLinkedList.java
+++ LinkedLists/DoublyLinkedLists/doublyLinkedList.java
@@ -1,6 +1,7 @@
 package LinkedLists.DoublyLinkedLists;
+import LinkedLists.SinglyLinkedList;
 
-public class doublyLinkedList{
+public class doublyLinkedList extends SinglyLinkedList{
 
     public Node head;        //3 instance variables.
     public Node tail;
@@ -33,4 +34,75 @@ public class doublyLinkedList{
         return this.length;
     }
 
+    public void displayForward(){     // Traverse linkedlist in forward direction
+        if(head == null){
+            return;
+        }
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+
+    public void displayBackward(){           // Traverse linked list backward.
+        if(tail == null){
+            return;
+        }
+        
+        Node temp = tail;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.prev;
+        }
+        System.out.println("Null");
+    }
+
+    public void insertAtFirst(int value){
+        Node newNode = new Node(value);
+        if(isEmpty()){
+            tail = newNode;
+        }
+        else{
+            head.prev = newNode;
+        }
+        newNode.next = head;
+        head = newNode;
+        length++;
+    }
+
+    public void insertAtLast(int value){
+        Node newNode = new Node(value);
+        if(isEmpty()){
+            head = newNode;
+        }
+        else{
+            tail.next = newNode;
+            newNode.prev = tail;
+        }
+        tail = newNode;
+        length++;
+    }
+
+
+    public static void main(String[] args) {
+        doublyLinkedList dll = new doublyLinkedList();
+
+        //Insert at beginning.
+        dll.insertAtFirst(10);
+        dll.insertAtFirst(20);
+        dll.insertAtFirst(30);
+        dll.displayForward(); // Output: 30->20->10->Null
+
+        //Insert at last.
+        dll.insertAtLast(40);
+        dll.insertAtLast(50);
+        dll.displayForward(); // Output: 30->20->10->40->50->Null
+
+        dll.insertAtFirst(60);
+        dll.displayForward(); // Output: 60->30->20->10->40->50->Null
+
+    }
 }
