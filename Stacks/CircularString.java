f70a26a474e2f62da5064b0b2f5111763b309611
KS MANOJ KUMAR <ksmk2104@gmail.com>
2024-11-07 22:41:01 +0530

    added extra method of implementation to circularstring.java

--- Stacks/CircularString.java
+++ Stacks/CircularString.java
@@ -2,9 +2,12 @@ package Stacks;
 
 public class CircularString {
     public static void main(String[] args) {
-        String sen = "leetcode";
-        isCircularSentence(sen);
-         
+        String sen = "leetcode el";
+        boolean b1 = isCircularSentence(sen);
+        boolean b2 = isCircularSentence2(sen);
+        System.out.println(b1);
+        System.out.println(b2);
+
     }
     public static boolean isCircularSentence(String sentence) {
         String[] words = sentence.split(" ");
@@ -18,4 +21,12 @@ public class CircularString {
         return true;
     }
 
+
+            public static boolean isCircularSentence2(String sentence)
+             {
+                for (int i = 0; i < sentence.length(); ++i){
+                     if (sentence.charAt(i) == ' ' && sentence.charAt(i - 1) != sentence.charAt(i + 1)) return false;
+                }
+                return sentence.charAt(0) == sentence.charAt(sentence.length() - 1);
+            }
 }
