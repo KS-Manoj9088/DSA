e358359939d31c424a8021bfa8e7c2cbb3739056
KS MANOJ KUMAR <ksmk2104@gmail.com>
2024-11-14 21:14:00 +0530

    Added Maximum Product SubArray

---
+++ Competative_Programming/MaxProductSubarray.java
@@ -0,0 +1,29 @@
+package Competative_Programming;
+
+public class MaxProductSubarray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        int max_product = maxProduct(arr);
+        System.out.println(max_product);
+
+    }
+
+    public static int maxProduct(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0;i < nums.length; i++){
+            int prod = 1;
+            for(int j = i;j < nums.length; j++){
+                prod = prod * nums[j];
+                if(prod > max){
+                max = prod;
+                }
+             }
+        }
+
+        return max;
+    }
+}
+
