de1651c3c16ce3a586fa1f377f8d4410074c975c
KS MANOJ KUMAR <ksmk2104@gmail.com>
2025-02-16 17:07:21 +0530

    Starting BackTracking and Solved Basic Maze Problems

---
+++ BackTracking/countpaths.java
@@ -0,0 +1,22 @@
+package BackTracking;
+
+public class countpaths{
+    public static void main(String[] args) {
+        int[] initialPos = new int[] {4,4};    //Telling that a 4*4 matrix is given and start position is => {4,4}
+        //int[] finalPos = new int[] {1,1};      //Final position of the maze is {1,1}
+        int totalPaths = countPaths(initialPos[0],initialPos[1]);
+        System.out.println("Total number of paths from start to end is: "+totalPaths);
+
+    }
+
+    public static int countPaths(int row, int col){
+        if(row==1 || col==1){
+            return 1;
+        }
+        int countDown = 0;
+        int countRight = 0;
+        countDown = countDown + countPaths(row-1,col);
+        countRight = countRight + countPaths(row,col-1);
+        return countDown + countRight;
+    }
+}
---
+++ BackTracking/printPaths.java
@@ -0,0 +1,62 @@
+package BackTracking;
+import java.util.*;
+
+public class printPaths {
+
+    public static void main(String[] args) {
+        int[] initialPos = new int[] {3,3};
+        ArrayList<String> paths = printpaths("", initialPos[0], initialPos[1]);
+        for (String path : paths) {
+            System.out.println(path);
+        }
+    }
+
+    // Steps to return a ArrayList
+    // 1. Maintain a local ArrayList
+    // 2. on the local list use "addAll" method on left-recursion calls and right-recursion calls
+    // 3. return local list.
+    // 4. In case of base condition Add result to list and return list.
+
+    public static ArrayList<String> printpaths(String p, int row, int col){
+        ArrayList<String> paths = new ArrayList<>();
+        if(row == 1 && col == 1){
+            paths.add(p);
+            return paths;
+        }
+        if(col>1){
+            paths.addAll(printpaths(p + "R", row, col-1));
+        }
+
+        if(row>1){
+            paths.addAll(printpaths(p + "D", row-1, col));
+        }
+        return paths;
+    }
+
+    /*
+     *  public static ArrayList<String> printPaths1(String p, int row, int col) {
+        ArrayList<String> paths = new ArrayList<>();
+
+        // Base Case: If row or col reaches 1, construct the remaining path
+        if (row == 1 || col == 1) {
+            while (row > 1) {
+                p += "D";
+                row--;
+            }
+            while (col > 1) {
+                p += "R";
+                col--;
+            }
+            paths.add(p);
+            return paths;
+        }
+
+        paths.addAll(printPaths1(p + "R", row, col - 1));
+
+        // Move Down
+        paths.addAll(printPaths1(p + "D", row - 1, col));
+
+        return paths;
+    }
+    */       
+}
