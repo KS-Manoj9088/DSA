79a7d39c04cad11f980193df2503c521f8f5c828
KS MANOJ KUMAR <ksmk2104@gmail.com>
2025-03-17 22:07:09 +0530

    .......

--- Strings/LongestIncreasingSubsequence.java
+++ Strings/LongestIncreasingSubsequence.java
@@ -4,7 +4,7 @@ public class LongestIncreasingSubsequence{
     public static int lengthOfLIS(int[] nums) {
         int[] dp = new int[nums.length];
         int[] subseq = new int[nums.length];
-        int max_length = 0;
+        //int max_length = 0;
         for(int i = 1;i<nums.length;i++){
             for(int j = 0;j<i;j++){
                 if(nums[i]>nums[j]){
