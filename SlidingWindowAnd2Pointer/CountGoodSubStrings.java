7bdbf2edb186ce914d704f91d31833741ffb53cf
KS MANOJ KUMAR <ksmk2104@gmail.com>
2025-03-23 06:54:42 +0530

    Wokrd on Optimal approach for COunt good strinsgs

--- SlidingWindowAnd2Pointer/CountGoodSubStrings.java
+++ SlidingWindowAnd2Pointer/CountGoodSubStrings.java
@@ -1,8 +1,15 @@
 package SlidingWindowAnd2Pointer;
 
+import java.util.HashMap;
 
 // LEETCODE 1876
 public class CountGoodSubStrings {
+
+    public static void main(String[] args) {
+        String str = "xyaabcaa";
+        int goodStringsCount = countGoodSubstrings(str);
+        System.out.println("Good Substrings Count: " + goodStringsCount); //3 ["xya", "abc", "bca"]
+    }
     
 
     public static int countGoodSubstrings(String s) {
@@ -18,6 +25,34 @@ public class CountGoodSubStrings {
                 count++;
             }
         }
+        return count;
+    }
+
+    public static int optimalApproach(String s){
+        int count = 0;
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        int left = 0;
+        for(int right = 0; right < s.length();right++){
+
+            char ch = s.charAt(right);
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+
+            if(right-left == 3){
+                char ch1 = s.charAt(left);
+                hm.put(ch1, hm.get(ch1)-1);
+
+                if(hm.get(ch1)==0){
+                    hm.remove(ch1);
+                }
+                left++;
+            }
+
+            if(hm.size()==3){
+                count++;
+            }
+        }
+        
         return count;
     }
 }
