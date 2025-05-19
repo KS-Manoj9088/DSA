d0b0d72d0acad84243ef5af2880aeb874692d718
KS MANOJ KUMAR <ksmk2104@gmail.com>
2025-05-19 12:51:47 +0530

    Solved Some Package Level Importing Conflicts

--- Arrays/Dummy.java
+++ Arrays/Dummy.java
@@ -1,6 +1,7 @@
 package Arrays;
 import BinarySearch.binarySearch;
-public class Dummy {
+
+public class Dummy{
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8};
         binarySearch bs = new binarySearch();
--- BinarySearch/binarySearch.java
+++ BinarySearch/binarySearch.java
@@ -4,11 +4,11 @@ public class binarySearch{
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7};
         int target = 5;
-        int result = binarySearching(arr, target);
+        int result = new binarySearch().binarySearching(arr, target);
         System.out.println("The index of target is : "+result);
     }
 
-    public static int binarySearching(int arr[],int target){
+    public int binarySearching(int arr[],int target){
         int low = 0;
         int high = arr.length-1;
         while(low<=high){
